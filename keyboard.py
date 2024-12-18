e697e9b4123afe7295d4b0aeeb4d04fa88846746
vlad4 <vlad4.efremova@yandex.ru>
2024-12-18 21:00:53 +0300

    изменила кнопки

--- keyboard.py
+++ keyboard.py
@@ -1,8 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 star =  InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text = 'IT', callback_data='IT')],
-    [InlineKeyboardButton(text = 'Музыка',callback_data='Music')],
-    [InlineKeyboardButton(text = 'Искусство',callback_data='Art')],
-    [InlineKeyboardButton(text = 'Политика',callback_data='Politics')]
+    [InlineKeyboardButton(text = 'Технологии', callback_data='technology')],
+    [InlineKeyboardButton(text = 'Спорт',callback_data='sports')],
+    [InlineKeyboardButton(text = 'Политика',callback_data='Politics')],
+    [InlineKeyboardButton(text = 'Топ',callback_data='top')],
+    [InlineKeyboardButton(text = 'Бизнес',callback_data='business')],
+    [InlineKeyboardButton(text = 'Наука',callback_data='science')],
 ])
